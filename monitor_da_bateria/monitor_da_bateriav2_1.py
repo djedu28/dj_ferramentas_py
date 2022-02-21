3f8514677d1ff362b2be60b4193ff38a904558d2
Eduardo Santos <43933482+djedu28@users.noreply.github.com>
2022-02-21 02:59:59 -0300

    Monitor da bateria V3.0- Inserindo musica nas notificações
    Melhoria nas notificações usando arquivos de musica para os sons
    
    novidades:
        * Inserindo musica para as notificações
    
    já implementado:
        * Ler dados da bateria
        * Informar por som mudança de estado
        * colorindo a tela
        * Diferenciando notificação pelo som
        * Melhorando notificação sonora (bipes diferentes)

--- monitor_da_bateria/monitor_da_bateriav2_1.py
+++ monitor_da_bateria/monitor_da_bateriav2_1.py
@@ -1,32 +1,34 @@
 # @DjEdu28 - Licença MIT - Copyright (c) 2022 @DjEdu28 (Luis Eduardo Silva dos Santos) 
 """
 Criado por @DjEdu28 EM: 01/2022
-Atualizado em: 02/2022
+Atualizado em: 01/2022
 Monitor da bateria
-versão 3.0
+versão 2.1
 
 Objetivo da ferramenta:
     Observar a porcentagem da bateria e informar se tá descarregando ou carregando
     Ainda aletar caso chegue em um nivel critico de carga
     
 novidades:
-    * Inserindo musica para as notificações
+    * Melhorando notificação sonora
     
 já implementado:
     * Ler dados da bateria
     * Informar por som mudança de estado
     * colorindo a tela
     * Diferenciando notificação pelo som
-    * Melhorando notificação sonora (bipes diferentes)
     
 Ps.:
-   Melhoria nas notificações usando arquivos de musica para os sons
+   Melhoria nas notificações usando notas/tons diferentes
 """
 import psutil
 from time import sleep
 from os import system as sys
-from winsound import PlaySound
 
+# def secs2hours(secs):
+    # mm, ss = divmod(secs, 60)
+    # hh, mm = divmod(mm, 60)
+    # return "%d:%02d:%02d" % (hh, mm, ss)
 notas = {
     "do" : 261,
     "re" : 293,             
@@ -36,21 +38,6 @@ notas = {
     "la" : 0,
     "si" : 0,          
 } 
-
-cima = r".\audio\[cima]mixkit-positive-notification-951.wav"
-baixo = [
-    r".\audio\[baixo]mixkit-wrong-answer-fail-notification-946.wav",
-    r".\audio\[baixo]mixkit-software-interface-remove-2576.wav",
-    r".\audio\[baixo]mixkit-game-notification-wave-alarm-987.wav",
-]
-
-# def alerta(nota="fa"):
-    # from time import sleep
-    # print('\a') # som de notificação
-    # for i in range(4):
-        # tone(notas[nota]*(i+1),300)
-        # sleep(0.200)
-
 def tone(freq,duration):
     from winsound import Beep
     # duration = 1000  # milliseconds
@@ -76,25 +63,22 @@ def main():
       if battery.percent < 50 and battery.power_plugged == False:
         sys("color 47")
         # [tone(3300,200*n) for n in range(1,4)]
-        #alerta()
-        PlaySound(baixo[1],0)
+        alerta()
         print("Bateria baixa",sep="")
         
       elif (battery.power_plugged == False and avisos<3):
         sys("color 67")
         #print('\a'+"Plug desconectado")
         #tone(4000,400)
-        #alerta(nota="do")
+        alerta(nota="do")
         print('\t'+"Plug desconectado")
-        PlaySound(baixo[0],0)
         avisos+=1
       elif (battery.power_plugged and avisos>=3):
         avisos = 0
         #tone(1100,200);tone(1100,300)
-        #alerta(nota="do")
+        alerta(nota="do")
         #print("\a Carregando...")
         print("\tCarregando...")
-        PlaySound(cima,0)
         sys("color 2F")
       elif (battery.percent > 50 and battery.power_plugged):
         sys("color 0F")
